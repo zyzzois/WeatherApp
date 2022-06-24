f7e56cea3802ea12626207a0db7049f9504d5813
Octane Silvio <ruslangibadullin70@gmail.com>
2022-06-24 17:53:12 +0500

    added ViewBinding, XML, onClickListener, url, receiving a request by VolleyClass(VolleyCalss included in gradle), obtained permission to use the Internet in manifest. JSonobject Class.

--- app/src/main/java/com/octaneocatane/weatherapp/MainActivity.kt
+++ app/src/main/java/com/octaneocatane/weatherapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.octaneocatane.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.octaneocatane.weatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
 
+const val API_KEY = "be8f4a5e6d3d4498b73182435222306"
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonGet.setOnClickListener {
+            getResult("London")
+        }
+    }
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json" +
+                "?key=$API_KEY&q=$name&aqi=no\n"
+
+        //новая очередь запроса
+        val queue = Volley.newRequestQueue(this)
+        //создаем новый запрос
+        val stringRequest = StringRequest(Request.Method.GET, url, //мы хотим получить поэтому используем метод GET
+            {
+                response->
+                val obj = JSONObject(response)
+                val temp = obj.getJSONObject("current")
+                Log.d("MyLog", "Response: ${temp.getString("temp_c")}")
+            },
+            {
+                Log.d("MyLog", "Volley error: $it")
+            }
+        )
+        queue.add(stringRequest)
     }
 }
